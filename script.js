a5b9891578f26d3e4eb8cb68238bb9a508f9d541
sherary <67370125+sherary@users.noreply.github.com>
2020-07-16 08:45:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -63,6 +63,9 @@ const calculate = () => {
         case "/":
             result = parseFloat(prevNumber) / parseFloat(currentNumber)
             break
+        case "%":
+            result = parseFloat(prevNumber) * parseFloat(currentNumber) / 100
+            break
         default:
             break
     }
